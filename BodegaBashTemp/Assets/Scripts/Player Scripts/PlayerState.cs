cb19590dd7c119458cd19b83dfbd16dc4a17f3f6
u0876710 <u0876710@fmab-109-117.finearts.utah.edu>
2017-03-02 15:52:22 -0700

    ported over scripts and assets

--- BodegaBashTemp/Assets/Scripts/Player Scripts/PlayerState.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class PlayerState {
-
-    public abstract void Start(PlayerController player);
-    public abstract void Update(PlayerController player);
-    public abstract void End(PlayerController player);
-}
