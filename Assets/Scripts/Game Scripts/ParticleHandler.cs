c8cdabcc39330e600bf0b10c1a3b33b820725615
Jake Reuling <jacobreuling@gmail.com>
2017-04-04 16:00:10 -0600

    Minor Tweaks
    -Moved KillPlane script to Scripts folder
    -Tweaked ParticleHandler script
    -Removed extra directional lights from Bridges and OutdoorMarket maps
    -Created Kill Plane prefab and added it to OutdoorMarket map

--- Assets/Scripts/Game Scripts/ParticleHandler.cs
+++ Assets/Scripts/Game Scripts/ParticleHandler.cs
@@ -10,22 +10,26 @@ public class ParticleHandler : MonoBehaviour {
 	}
 
 	public static void SpawnDust( PlayerController player, string dustState, Vector3 dustRotation ) {
-		float sizeMultiplier;
-		if (dustState == "landing") 
-			sizeMultiplier = Clamp(Mathf.Abs(player.currSpeed.y), 1, 20)*0.05f;
+		float sizeMultiplier = 0.5f;
+		float startSpeed = 20f;
+		var dustOrigin = new Vector3(player.origin.position.x, player.origin.position.y - 1.1f, player.origin.position.z);
+		if (dustState == "landing") {
+			sizeMultiplier = Clamp (Mathf.Abs (player.currSpeed.y), 0.5f, 20) * 0.05f;
+			startSpeed = Clamp (Mathf.Abs (player.currSpeed.y), 1, 50);
+		}
 		else if (dustState == "jumping")
 			sizeMultiplier = 0.5f;
-		else
-			sizeMultiplier = 0.5f;
-		ParticleSystem jumpdust = UnityEngine.Object.Instantiate(player.JumpDust, player.origin.position, Quaternion.LookRotation(dustRotation));
+
+		ParticleSystem jumpdust = UnityEngine.Object.Instantiate(player.JumpDust, dustOrigin, Quaternion.LookRotation(dustRotation));
 		var jumpdustMain = jumpdust.main; 
 		var jumpdustShape = jumpdust.shape;
 
 		jumpdustMain.startSizeMultiplier = sizeMultiplier;
 		jumpdustShape.radius = sizeMultiplier;
+		jumpdustMain.startSpeed = startSpeed;
 		//Debug.Log ("dustSize = " + sizeMultiplier);
 	}
-
+		
 	public static float Clamp(float num, float min, float max){
 		if (num < min)
 			num = min;
